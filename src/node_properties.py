808a4cffc7cf04615992e58c5223a918c25d039d
Ankur Ankan <ankurankan@gmail.com>
2014-01-14 09:49:35 +0530

    linked list traverse backend prototype

--- src/node_properties.py
+++ src/node_properties.py
@@ -1,40 +1,37 @@
 import matplotlib
-import nx_animation as nx_anim
-
-matplotlib.use('Agg')
-import matplotlib.pyplot as plt
-
-__author__ = 'Ankur Ankan'
-
-step = 0
-trace = {}
+import networkx as nx
+import json
 
 
 def get_state(G):
     state = {'nodes': G.nodes(), 'edges': G.edges(), 'edge_color': {}, 'edge_line_width': {}, 'edge_line_style': {},
              'edge_alpha': {}, 'node_face_color': {}, 'node_edge_color': {}, 'node_size': {}, 'node_style': {},
              'node_alpha': {}}
-
-    # TODO: Add a check if graph has already been plotted
-    # Considering that graph has been plotted
-    axes = plt.gca()
-    number_of_nodes = G.number_of_nodes()
-
-    node_collection = axes.get_children()[number_of_nodes + 2]
-    node_face_color = node_collection.get_facecolor().tolist()
-    if len(node_face_color) == 1:
-        for node in G.nodes():
-            state['node_face_color'][node] = node_face_color[0]
-    else:
-        for i in range(number_of_nodes):
-            state['node_face_color'][G.nodes[i]] = node_face_color[i]
-
+    for node in G.nodes():
+        for prop, value in G.node[node].items():
+            state[prop][node] = value
     return state
 
 
-def neighbours(G, color):
-    neighbours = G.neighbours()
-    for node in neighbours:
-        nx_anim.set_node_facecolor(G, node=node, color=color)
+def traverse_linked_list(linked_list):
+    """
+    Linked List in form of [(1,2), (2,3), (4,5), (5,6)]
+    """
+    trace = {}
+    G = nx.DiGraph(linked_list)
+    step = 0
+    start = linked_list[0][0]
+    G.node[start]['node_face_color'] = 'blue'
     trace[step] = get_state(G)
     step += 1
+    end = linked_list[-1][-1]
+    while start != end:
+        start = G.neighbors(start)[0]
+        G.node[start]['node_face_color'] = 'blue'
+        trace[step] = get_state(G)
+        step += 1
+    print(json.dumps(trace))
+
+if __name__ == '__main__':
+    linked_list = [(1,2), (2,3), (3,4), (4,5), (5,6)]
+    traverse_linked_list(linked_list)
