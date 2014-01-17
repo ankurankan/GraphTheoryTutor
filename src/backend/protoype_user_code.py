645817e582d9998d51bd79a7bac7da8e850432c9
Ankur Ankan <ankurankan@gmail.com>
2014-01-17 12:38:33 +0530

    adds functions for checking node assignment and identifying those nodes

--- src/backend/protoype_user_code.py
+++ src/backend/protoype_user_code.py
@@ -19,9 +19,30 @@ class LinkedList:
 
 
 class Visualize(bdb.Bdb):
-    #Experimenting with bdb module
-    pass
 
+    def check_var_assignment(self, frame):
+        variables = frame.f_code.co_varnames
+        node_variables = []
+        for var in variables:
+            if isinstance(frame.f_locals[var], Node):
+                node_variables.append(var)
+        self.identify_nodes(node_variables)
+
+    def identify_nodes(self, node_variables):
+        node_id = self.dict_node_id()
+        node_objects = []
+        for var in node_variables:
+            node_objects.append(node_id[var])
+        return node_objects
+
+    def dict_node_id(self):
+        node_id = {}
+        curr_node = linked_list.head
+        node_id[id(curr_node)] = curr_node
+        while curr_node:
+            curr_node = curr_node.next
+            node_id[id(curr_node)] = curr_node
+        return node_id
 
 if __name__ == '__main__':
     code = """node = head\nwhile node != None:\n\tnode = node.next"""
