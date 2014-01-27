9f158ae39754fc6f7b6a93eaeeb64244e429fb41
Ankur Ankan <ankurankan@gmail.com>
2014-01-27 12:13:35 +0530

    adds an example with manually extracting frames

--- src/backend/protoype_user_code.py
+++ src/backend/protoype_user_code.py
@@ -1,5 +1,27 @@
 import bdb
 
+############################## Example with manually extracting frames #################################################
+# >>> def traverse():
+# ...     linked = LinkedList([1,2,3,4,5,6,7,8,9])
+# ...     curr = linked.head
+# ...     print(sys._getframe(0).f_locals)
+# ...     while curr:
+# ...             curr = curr.next
+# ...             print(sys._getframe(0).f_locals)
+# ...
+# >>> traverse()
+# {'linked': <__main__.LinkedList object at 0x7f04c9ceeed0>, 'curr': <__main__.Node object at 0x7f04c9cee790>}
+# {'linked': <__main__.LinkedList object at 0x7f04c9ceeed0>, 'curr': <__main__.Node object at 0x7f04c9ceec50>}
+# {'linked': <__main__.LinkedList object at 0x7f04c9ceeed0>, 'curr': <__main__.Node object at 0x7f04c9cf3190>}
+# {'linked': <__main__.LinkedList object at 0x7f04c9ceeed0>, 'curr': <__main__.Node object at 0x7f04c9cf3850>}
+# {'linked': <__main__.LinkedList object at 0x7f04c9ceeed0>, 'curr': <__main__.Node object at 0x7f04c9cf3410>}
+# {'linked': <__main__.LinkedList object at 0x7f04c9ceeed0>, 'curr': <__main__.Node object at 0x7f04c9cf3390>}
+# {'linked': <__main__.LinkedList object at 0x7f04c9ceeed0>, 'curr': <__main__.Node object at 0x7f04c9cf3450>}
+# {'linked': <__main__.LinkedList object at 0x7f04c9ceeed0>, 'curr': <__main__.Node object at 0x7f04c9cf3490>}
+# {'linked': <__main__.LinkedList object at 0x7f04c9ceeed0>, 'curr': <__main__.Node object at 0x7f04c9cf34d0>}
+# {'linked': <__main__.LinkedList object at 0x7f04c9ceeed0>, 'curr': None}
+########################################################################################################################
+
 
 class Node:
     def __init__(self, data):
@@ -34,8 +56,9 @@ class Visualize(bdb.Bdb):
         for var in node_variables:
             node_objects.append(node_id[var])
         return node_objects
-
-    def dict_node_id(self):
+   
+    @staticmethod
+    def dict_node_id():
         node_id = {}
         curr_node = linked_list.head
         node_id[id(curr_node)] = curr_node
